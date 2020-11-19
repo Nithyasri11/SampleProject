04650cf4f9fbbc23e1e8b29271c39724b410438d
Nithyasri11 <nithigugan91@gmail.com>
2020-11-19 06:52:01 +0100

    Initial stage project

---
+++ src/test/java/org/cap/MavenProject.java
@@ -0,0 +1,12 @@
+package org.cap;
+
+import org.base.LibGlobal;
+
+public class MavenProject extends LibGlobal{
+	public static void main(String[] args) {
+		LibGlobal lib = new LibGlobal();
+		
+		
+	}
+	
+}
